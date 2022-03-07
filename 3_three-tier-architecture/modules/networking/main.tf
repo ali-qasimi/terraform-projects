16f9d5ce03ed2b0b05f51fe0544dae9b5a719607
Ali <alisina_q@yahoo.com.au>
2022-03-07 22:50:55 +1100

    adding SGs

--- 3_three-tier-architecture/modules/networking/main.tf
+++ 3_three-tier-architecture/modules/networking/main.tf
@@ -3,7 +3,13 @@ data "aws_availability_zones" "available" {
 } 
 
 locals {
-    availability_zones     = data.aws_availability_zones.available.names
+    availability_zones = data.aws_availability_zones.available.names
+}
+
+variable "all_security_groups" {
+    description = "list of security groups to create"
+    type = list(string)
+    default = ["webserver_alb_sg", "webserver_sg", "appserver_alb_sg", "appserver_sg", "db_sg"]
 }
 
 resource "aws_vpc" "application_vpc" {
@@ -36,3 +42,46 @@ resource "aws_subnet" "all_database_subnets" {
     cidr_block          = cidrsubnet(aws_vpc.application_vpc.cidr_block, 16, count.index+21)
     availability_zone   = local.availability_zones[count.index]
 }
+
+resource "aws_security_group" "http_80_inbound" {
+    name        = "http_80_inbound"
+    description = "allows inbound http traffic"
+    vpc_id      = aws_vpc.application_vpc.id
+
+    ingress {
+        from_port   = 80
+        to_port     = 80
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+
+resource "aws_security_group" "sg_for_all" {
+    
+    for_each = toset(var.all_security_groups)
+    
+    name = each.value
+    description = "security group for ${each.value}"
+    vpc_id = aws_vpc.application_vpc.id
+    ingress {
+        from_port       = 0
+        to_port         = 0
+        protocol        = -1
+        security_groups = [aws_security_group.http_80_inbound.id]
+    }
+}
+
+
+
+/* resource "aws_security_group" "db_sg" {
+    name        = "db_sg"
+    description = "allows inbound traffic into the mySQL DB"
+    vpc_id      = aws_vpc.application_vpc.id
+
+    ingress {
+        from_port   = 80
+        to_port     = 80
+        protocol    = tcp
+        cidr_blocks = "0.0.0.0/0"
+    }
+} */
